38c18b7c86a8776d339022419fda302385e58d6b
yuriykudreiko <yuriykudreiko@gmail.com>
2018-12-04 22:19:38 +0300

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Yura on 12/4/18.
+//  Copyright © 2018 Yuriy Kudreika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
